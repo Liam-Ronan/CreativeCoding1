d3242f583b49f55fdef84efa254a3b4759202a2e
Liam Ronan (Student) <N00212101@iadt.ie>
2023-02-03 14:50:01 +0000

    added scaling to bar graph

--- Class 01 - Start/sketch.js
+++ Class 01 - Start/sketch.js
@@ -7,8 +7,13 @@
 
 scream(5, "help"); */
 
-//Chart
-let numBlocks = 10;
+//declaring array
+let data = [23,56,75,172,250,300,400,504,602,684];
+let maxValue = Math.max(...data);
+console.log(maxValue);
+
+//declaring variables
+let numBlocks = data.length;
 let chartWidth = 400;
 let chartHeight = 400;
 let marginLeft = 10;
@@ -16,14 +21,33 @@ let marginRight = 10;
 let blockGap = 20;
 let screenWidth = 500;
 let screenHeight = 500;
+
+
+
+//calculations
 let blockWidth = (chartWidth - (marginLeft + marginRight) - ((numBlocks-1)*blockGap))/numBlocks;
 
 let firstBlockxPos = ((screenWidth-chartWidth)/2)+marginLeft;
 
 let masterGap = blockWidth+blockGap;
 
-console.log(blockWidth);
+//squarer function
+/* function squarer(_num) {
+    let squared = _num * _num;
 
+    return squared;
+}
+
+squarer(3); */
+
+function scaler(_num) {
+    let scaleValue = chartHeight/maxValue;
+
+    return _num*scaleValue;
+}
+
+
+//Main Function to setup and draw actual bar chart
 function setup() {
     createCanvas(screenWidth, screenHeight);
     background(200);
@@ -34,10 +58,10 @@ function setup() {
 function draw() {
     background(200);
     fill(0);
-    for(let x=0; x < numBlocks; x++) {
+    for(let x=0; x < data.length; x++) {
         push();
-        translate(firstBlockxPos + (x*masterGap), chartHeight);
-        rect(0,0,blockWidth, random(-50,-400));
+        translate(firstBlockxPos + (x*masterGap), 450);
+        rect(0,0,blockWidth, scaler(-data[x]));
         pop();
     }
    
